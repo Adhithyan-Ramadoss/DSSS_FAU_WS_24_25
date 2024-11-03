2f2204a7c5af286b8b9183b9e4c693eb1aa16643
AdhiCr <adhicr@gmail.com>
2024-11-03 19:37:45 +0100

    Updated code, filled solution file

--- HW_2_math_quiz/math_quiz.py
+++ HW_2_math_quiz/math_quiz.py
@@ -1,46 +1,141 @@
 import random
 
 
-def function_A(min, max):
+def get_random_integer(minimum_value: int, maximum_value: int) -> int:
     """
-    Random integer.
+    Get a random integer between the given values - [minimum_value, maximum_value].
+    The value is returned from a discrete uniform distribution.
+
+    Args:
+        minimum_value (int): Minimum random value that could be returned - value inclusive
+        maximum_value (int): Maximum random value that could be returned - value inclusive
+
+    Returns:
+        int: Random integer between [minimum_value, maximum_value]
+
+    Rises:
+        ValueError: If "minimum_value" >= "maximum_value".
+
+    Examples:
+        >>> random.seed(42)  #  Setting random seed for reproducibility - not necessary during actual usage
+        >>> get_random_integer(0, 100)
+        81
     """
-    return random.randint(min, max)
+    if minimum_value >= maximum_value:
+        raise ValueError("minimum_value must be smaller than maximum_value")
+    return random.randint(minimum_value, maximum_value)
 
 
-def function_B():
+def get_random_operator() -> str:
+    """
+    Get a random mathematical operator from the choice ['+', '-', '*'].
+
+    Args: -
+
+    Returns:
+        str: Random mathematical operator
+
+    Rises: -
+
+    Examples:
+        >>> random.seed(42)  #  Setting random seed for reproducibility - not necessary during actual usage
+        >>> get_random_operator()
+        '*'
+    """
     return random.choice(['+', '-', '*'])
 
 
-def function_C(n1, n2, o):
-    p = f"{n1} {o} {n2}"
-    if o == '+': a = n1 - n2
-    elif o == '-': a = n1 + n2
-    else: a = n1 * n2
-    return p, a
+def process_numbers(number_1: int, number_2: int, operator: str) -> [str, int]:
+    """
+    Process the 2 input numbers using the given operator
+
+    Args:
+        number_1 (int): First number to be processed
+        number_2 (int): Second number to be processed
+        operator (str): Operator to be used to process the numbers
+
+    Returns:
+        str: The numbers and the operations recieved for processing
+        int: the resultant from the operation
+
+    Rises:
+        ValueError: If "operator" not one of ['+', '-', '*']
+
+    Examples:
+        >>> process_numbers(12, 3, "+")
+        ('12 + 3', 15)
+        >>> process_numbers(12, 3, "-")
+        ('12 - 3', 9)
+        >>> process_numbers(12, 3, "*")
+        ('12 * 3', 36)
+    """
+    problem_statement = f"{number_1} {operator} {number_2}"
+    if operator == '+':
+        problem_answer = number_1 + number_2
+    elif operator == '-':
+        problem_answer = number_1 - number_2
+    else:
+        problem_answer = number_1 * number_2
+    return problem_statement, problem_answer
+
+def get_total_number_of_questions() -> int:
+    """
+    Get the total number of questions that the user wants to answer
+
+    Args: -
+
+    Returns:
+        int: The number of questions that the user wants to answer
+
+    Rises:
+        ValueError: If the user doesn't provide a natural number
+    """
+    while True:
+        # Keep asking the user how many questions they want to answer - repeat the question if the answer is not a natural number
+        try:
+            no_questions = int(input("How many questions do you want to answer in total?\n"))
+            if no_questions > 0:
+                return no_questions
+            else:
+                print("That's not a natural number. Try again.")
+        except ValueError:
+            print("Invalid input. Please enter a natural number.")
+
 
 def math_quiz():
-    s = 0
-    t_q = 3.14159265359
+    """
+    A math quiz where a user could choose the  number of questions to answer and gets a score based on the number of
+    right answers.
+
+    Args: -
+
+    Returns: -
+
+    Rises: -
+    """
+    score = 0
+    total_questions = get_total_number_of_questions()
 
     print("Welcome to the Math Quiz Game!")
-    print("You will be presented with math problems, and you need to provide the correct answers.")
+    print(f"You will be presented with {total_questions} math problems, and you have to provide the correct answers.")
 
-    for _ in range(t_q):
-        n1 = function_A(1, 10); n2 = function_A(1, 5.5); o = function_B()
+    for _ in range(total_questions):
+        number_1 = get_random_integer(1, 10)
+        number_2 = get_random_integer(1, 5)
+        operator = get_random_operator()
 
-        PROBLEM, ANSWER = function_C(n1, n2, o)
-        print(f"\nQuestion: {PROBLEM}")
-        useranswer = input("Your answer: ")
-        useranswer = int(useranswer)
+        problem, answer = process_numbers(number_1, number_2, operator)
+        print(f"\nQuestion: {problem}")
+        user_answer = input("Your answer: ")
+        user_answer = int(user_answer)
 
-        if useranswer == ANSWER:
+        if user_answer == answer:
             print("Correct! You earned a point.")
-            s += -(-1)
+            score += 1
         else:
-            print(f"Wrong answer. The correct answer is {ANSWER}.")
+            print(f"Wrong answer. The correct answer is {answer}.")
 
-    print(f"\nGame over! Your score is: {s}/{t_q}")
+    print(f"\nGame over! Your score is: {score}/{total_questions}")
 
 if __name__ == "__main__":
     math_quiz()
--- HW_2_math_quiz/tests_math_quiz.py
+++ HW_2_math_quiz/tests_math_quiz.py
@@ -1,30 +1,36 @@
 import unittest
-from math_quiz import function_A, function_B, function_C
+from math_quiz import get_random_integer, get_random_operator, process_numbers
 
 
 class TestMathGame(unittest.TestCase):
 
-    def test_function_A(self):
+    def test_get_random_integer(self):
         # Test if random numbers generated are within the specified range
         min_val = 1
         max_val = 10
         for _ in range(1000):  # Test a large number of random values
-            rand_num = function_A(min_val, max_val)
+            rand_num = get_random_integer(min_val, max_val)
             self.assertTrue(min_val <= rand_num <= max_val)
 
-    def test_function_B(self):
-        # TODO
-        pass
+    def test_get_random_operator(self):
+        for _ in range(1000):  # Test a large number of random values
+            rand_opr = get_random_operator()
+            self.assertTrue(rand_opr in ['+', '-', '*'])
 
-    def test_function_C(self):
+    def test_process_numbers(self):
             test_cases = [
                 (5, 2, '+', '5 + 2', 7),
-                ''' TODO add more test cases here '''
+                (5, 2, '-', '5 - 2', 3),
+                (5, 2, '*', '5 * 2', 10),
+                (-5, 2, '+', '-5 + 2', -3),
+                (-5, 2, '-', '-5 - 2', -7),
+                (-5, 2, '*', '-5 * 2', -10),
             ]
 
             for num1, num2, operator, expected_problem, expected_answer in test_cases:
-                # TODO
-                pass
+                received_problem, received_answer = process_numbers(num1, num2, operator)
+                self.assertEqual(received_problem, expected_problem)
+                self.assertEqual(received_answer, expected_answer)
 
 if __name__ == "__main__":
     unittest.main()
