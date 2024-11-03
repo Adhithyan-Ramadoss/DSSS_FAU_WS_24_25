0face2e1a6f377ae85f4c0aac2c94bb3bf60a8eb
AdhiCr <adhicr@gmail.com>
2024-11-03 19:51:02 +0100

    Updated the setup check, for the folder name changes

--- setup_check.py
+++ setup_check.py
@@ -1,12 +1,12 @@
-import hw_package.HW_2_math_quiz as mq
+from hw_2_math_quiz.math_quiz import *
 
-result = mq.get_random_integer(0, 100)
+result = get_random_integer(0, 100)
 print("Result of get_random_integer:", result)
 
-result = mq.get_random_operator()
+result = get_random_operator()
 print("Result of get_random_operator:", result)
 
-result = mq.process_numbers(12, 3, "+")
+result = process_numbers(12, 3, "+")
 print("Result of process_numbers:", result)
 
 # # Using the main_function
