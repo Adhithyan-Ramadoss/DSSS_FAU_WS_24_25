3838bd69d7c8a56671cf3e745da2000e6568a5c5
AdhiCr <adhicr@gmail.com>
2024-11-03 18:44:32 +0100

    modified setup.py files

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-    name="DSSS_Homework",
-    version="1.0.0",
-    scripts=["HW_2_math_quiz/math_quiz.py"],
+    name="hw_package",
+    version="0.1.0",
+    description="Package for Data Science Survival Skills Homework",
+    author='Ramadoss, Adhithyan',
+    author_email='adhithyan.ramadoss@fau.de',
+    packages=find_packages(),
+    install_requires=[]
+    # scripts=["HW_2_math_quiz/math_quiz.py"],
 )
---
+++ setup_check.py
@@ -0,0 +1,13 @@
+import hw_package.HW_2_math_quiz as mq
+
+result = mq.get_random_integer(0, 100)
+print("Result of get_random_integer:", result)
+
+result = mq.get_random_operator()
+print("Result of get_random_operator:", result)
+
+result = mq.process_numbers(12, 3, "+")
+print("Result of process_numbers:", result)
+
+# # Using the main_function
+# ex.main_function()
